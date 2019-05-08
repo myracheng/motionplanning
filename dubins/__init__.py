2d05c737335130f0850e199a8d93257199ccfa06
Myra Cheng <myracheng027@gmail.com>
2019-05-08 14:35:43 -0700

    got rid of timestep limit

--- dubins/__init__.py
+++ dubins/__init__.py
@@ -6,7 +6,6 @@ logger = logging.getLogger(__name__)
 register(
     id='Dubins-v0',
     entry_point='dubins.envs:DubinsEnv',
-    timestep_limit=1000,
     reward_threshold=1.0,
     nondeterministic = True,
 )
